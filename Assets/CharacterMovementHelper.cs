60a4db15f2b055536410d73efc3d5c85c4b3f97d
freylily <lilyfrey@posteo.de>
2023-03-17 11:47:03 +0100

    add project

---
+++ Assets/CharacterMovementHelper.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Unity.XR.CoreUtils;
+
+public class CharacterMovementHelper : MonoBehaviour
+{
+    private XROrigin XROrigin;
+    private CharacterController CharacterController;
+    private CharacterControllerDriver driver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCharacterController();
+    }
+
+
+    /// <summary>
+    /// Updates the <see cref="CharacterController.height"/> and <see cref="CharacterController.center"/>
+    /// based on the camera's position.
+    /// </summary>
+    protected virtual void UpdateCharacterController()
+    {
+        if (XROrigin == null || CharacterController == null)
+            return;
+
+        var height = Mathf.Clamp(XROrigin.CameraInOriginSpaceHeight, driver.minHeight, driver.maxHeight);
+
+        Vector3 center = XROrigin.CameraInOriginSpacePos;
+        center.y = height / 2f + CharacterController.skinWidth;
+
+        CharacterController.height = height;
+        CharacterController.center = center;
+    }
+
+}
