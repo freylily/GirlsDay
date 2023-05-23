e37c435a39a4274f4a3fda4947fa0e6abc3c4279
freylily <lilyfrey@posteo.de>
2023-05-23 13:22:33 +0200

    optimized for experiment

--- Assets/nextscene.cs
+++ Assets/nextscene.cs
@@ -8,7 +8,7 @@ public class nextscene : MonoBehaviour
 {
     public string scenename;
 
-    void OnTriggerStay()
+    void OnTriggerEnter()
     {
         SceneManager.LoadScene(scenename);
     }
